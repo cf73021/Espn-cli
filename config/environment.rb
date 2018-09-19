d118d7b5a04b7b841211f634e0355630806c515c
Chris Franklin <cf73021@gmail.com>
2018-09-19 21:31:34 +0000

    basic frameworks

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+##require_relative '../lib/worlds_best_restaurants/scraper'
+##require_relative '../lib/worlds_best_restaurants/restaurant'
+##require_relative '../lib/worlds_best_restaurants/cli'
+##require_relative '../lib/worlds_best_restaurants/version'
---
+++ lib/hbomovies.rb
@@ -0,0 +1,4 @@
+module Hbomovies
+end
+
+require_relative '..config/environment'
